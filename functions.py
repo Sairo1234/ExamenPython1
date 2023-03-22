25d5a8a190cd3f6a173e65222b141f4c09c944de
Sairo1234 <alejandronogbel@gmail.com>
2023-03-22 11:12:26 +0100

    commit_4

--- functions.py
+++ functions.py
@@ -5,19 +5,24 @@ def read_data(filename):
     """Esta funcion lee un fichero csv y devuelve un diccionario con los datos del fichero.
 
     Args:
-        filename (_type_): _description_
+        filename (text): nombre del fichero csv
 
     Raises:
-        ValueError: _description_
+        ValueError: Si el fichero tiene menos de 10 muestras con todos los atributos.
 
     Returns:
-        _type_: _description_
+        diccionario: los datos del fichero csv
     """
+    
     with open(filename, 'r') as file:
+        
         reader = csv.DictReader(file)
         data = {}
+        
         for row in reader:
+            
             if all(row.values()):
+                
                 data[f'dato{len(data)+1}'] = {
                     'type': row['type'],
                     'fixed acidity': row['fixed acidity'],
@@ -33,12 +38,66 @@ def read_data(filename):
                     'alcohol': row['alcohol'],
                     'quality': row['quality']
                 }
+                
         if len(data) < 10:
             raise ValueError('El fichero debe tener al menos 10 muestras con todos los atributos.')
+        
         return data
 
 #Ejercicio 3
+def split(data):
+    """Esta funcion recibe un diccionario con los datos del fichero csv y devuelve dos diccionarios, uno con los datos del tipo white y otro con los datos del tipo red.
 
+    Args:
+        data (diccionario): diccionario con los datos del fichero csv
 
-file = 'winequality.csv'
-data = read_data(file)
+    Returns:
+        diccionario: diccionario con los datos del tipo white
+        diccionario: diccionario con los datos del tipo red
+    """
+    
+    white = {}
+    red = {}
+    
+    for i in data:
+        
+        if data[i]['type'] == 'white':
+            white[i] = data[i]
+            del white[i]['type']
+            
+        elif data[i]['type'] == 'red':
+            red[i] = data[i]
+            del red[i]['type']
+            
+    return white, red
+
+#Ejercicio 4
+def reduce(diccionario, atributo):
+    """_summary_
+
+    Args:
+        diccionario (_type_): _description_
+        atributo (_type_): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    
+    lista = []
+    
+    for i in diccionario:
+        
+        if atributo in diccionario[i]:
+            lista.append(diccionario[i][atributo])
+            
+        else:
+            raise ValueError('El atributo no existe en el diccionario.')
+            
+    return lista
+
+#Ejercicio 5
+def silhouette(lista1, lista2):
+    
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
-from functions import read_data
+from functions import read_data, split, reduce
 
 #testear la funcion read_data
 
 file = 'winequality.csv'
 data = read_data(file)
-print(data)
+
+#testear la funcion split
+diccionario_white, diccionario_red = split(data)
+#print(diccionario_white)
+
+#testear la funcion reduce
+lista = reduce(diccionario_white, 'alcohol')
+print(lista)
